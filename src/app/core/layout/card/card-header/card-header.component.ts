f2a35b08741c5c4d76353d9d34caddac1aa0b47f
Alan Albuquerque <alan@paggtaxi.com.br>
2018-05-19 02:51:57 -0300

    montando estrutura de grids dos cards de temperatura

--- src/app/core/layout/card/card-header/card-header.component.ts
+++ src/app/core/layout/card/card-header/card-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-header',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/weather/components/weather-box-list/weather-box-list.component.ts
+++ src/app/weather/components/weather-box-list/weather-box-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather-box-list',
   templateUrl: './weather-box-list.component.html',
-  styleUrls: ['./weather-box-list.component.css']
+  styleUrls: ['./weather-box-list.component.scss']
 })
 export class WeatherBoxListComponent implements OnInit {
 
